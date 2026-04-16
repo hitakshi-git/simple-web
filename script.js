88abfb3b697022e9af11515b2863c696197d23ab
hitakshi-git <sharmahitakshi343@gmail.com>
2026-04-16 13:22:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Show messages when page loads
+alert ("Welcome to my Azure Website");
+
+// Change text dynamically
+document.addEventListener("DOMContentLoaded",function () {
+
+document.getElementById("title").innerHTML = "Hello from JavaScript";
+});
